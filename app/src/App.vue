ce4d5d8fe9d3fd0cb03026e40efc19569cc5f1c2
Brian Morrison II <brian@brianmorrison.me>
2019-10-05 17:06:02 -0500

    #2 - Started building out main layout for PoC

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,76 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+     <v-navigation-drawer app permanent class="app-drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title app-title">
+            APOSTLE
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
-      <HelloWorld/>
+      <v-app-bar class="address-bar">
+        <v-select
+          :items="httpMethods"
+          label="Method"
+          class="http-method-select"
+        ></v-select>
+        <v-text-field
+          label="API Endpoint Address"
+          ></v-text-field>
+         <!-- <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field> -->
+      </v-app-bar>
+      <v-navigation-drawer>
+        <v-list-item>
+          Test config settings here...
+        </v-list-item>
+
+        <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+        <HelloWorld/>
     </v-content>
   </v-app>
 </template>
@@ -30,7 +84,29 @@ export default {
     HelloWorld,
   },
   data: () => ({
-    //
+    items: [],
+    httpMethods: [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE'
+    ]
   }),
 };
 </script>
+
+<style scoped>
+.app-drawer {
+  background-color: #094074 !important;
+}
+.app-title {
+  color: white;
+}
+.address-bar {
+  z-index: 9999;
+}
+.http-method-select {
+  max-width: 200px;
+}
+</style>
