e2c33d3da220ed5dcbe69adabf8c3a733d16deb4
Brian Morrison II <brian@brianmorrison.me>
2019-10-06 22:24:17 -0500

    Further progress on base layout, AB#2

--- app/src/components/Main.vue
+++ app/src/components/Main.vue
@@ -1,55 +1,97 @@
 <template>
-  <v-container ma-0 pa-0 class="body-main-container">
+  <v-container ma-0 pa-0 class="body-main-container grid-wrapper">
+    <div class='grid-top'>
       <v-app-bar class="address-bar">
         <v-select
           :items="httpMethods"
           label="Method"
           class="http-method-select"></v-select>
         <v-text-field label="API Endpoint Address"></v-text-field>
-      </v-app-bar>
-    <div style="display: flex; flex-direction: row; width: 100%; height: 100%; position: absolute; align-items: stretch;">
-      <div class="left-pane">
-        left pane
-      </div>
-      <div class="right-pane">
-        right pane
-      </div>
+      </v-app-bar></div>
+    <div class="grid-left">
+       <v-expansion-panels multiple>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Params</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            A table of query parameters will go here.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Auth</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Authentication settings will go here.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Headers</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Key value pair of headers will be displayed here, as well as auto-generated headers.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Variables</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            A custom list of variables used in this request.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Body Templates</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Alternate body templates can be saved into this section
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+      </v-expansion-panels>
     </div>
+    <div class="grid-right">Request body will be editted here.</div>
   </v-container>
 </template>
+
+
 
 <script>
 export default {
   data: () => ({
-    items: []
+    items: [],
+    httpMethods: [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE'
+    ]
   }),
 };
 </script>
 
 <style scoped>
-.address-bar {
-  z-index: 9999;
+.grid-wrapper {
+  display: grid;
+  grid-auto-columns: min-content 1fr auto;
+  grid-auto-rows: min-content 1fr auto;
+  background-color: #eee;
 }
-.left-pane {
-  background-color: red;
-  flex: 20;
+.grid-top {
+  grid-column: 1 / 3;
 }
-.right-pane {
-  background-color: green;
-  flex: 80;
+.grid-left {
+  min-width: 450px;
+  padding: 10px;
+}
+.grid-right {
+  padding: 10px;
+}
+.http-method-select {
+  width: 200px;
 }
 .body-main-container {
   position: relative;
   height: 100%;
   align-items: stretch;
-}
-.body-sub-container {
-  height: 100%
-}
-.body-main-row {
-  height: 100%;
-}
-.body-container {
-  height: 100%;
 }
 </style>
