6fa0f1a3d98f930ddce99c6f331fb453ea2191c6
Brian Morrison II <brian@brianmorrison.me>
2019-10-12 12:26:37 -0500

    #17 - Removed Vuetify from main container

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Once you've joined, DM a mod to request contributor access to both the repositor
 Progress will be tracked via the following Trello board;
 
 [Project Apostle Backlog](https://trello.com/c/V7ITr2eA/1-about-project-apostle-getting-involved)
+
+
+## TODO: Add section on starting upp the app
--- app/src/main.js
+++ app/src/main.js
@@ -3,13 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import vuetify from './plugins/vuetify';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
@@ -17,6 +16,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount('#app');
